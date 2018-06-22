98fe1561f0e8ab7a9e0bba8935a1c710c05325fc
Evgenii Solntsev <thexardass@gmail.com>
2018-06-22 11:56:21 +0300

    fix errorrs

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -28,46 +28,4 @@ export default class Helper {
       .join('')
       .slice(1, -4)
   }
-
-  static decode (dec) {
-    var decoded = 0
-    while (dec) {
-      var alphabetPosition = alphabet.indexOf(dec[0])
-      var powerOf = dec.length - 1
-      decoded += alphabetPosition * (Math.pow(base, powerOf))
-      dec = dec.substring(1)
-    }
-    return decoded
-  }
-
-  static unhex (string) {
-    if (typeof string !== 'string') {
-      return ''
-    }
-    let res = ''
-    for (let i = 0; i < string.length; i += 2) {
-      const digit = parseInt(`0x${string.slice(i, i + 2)}`)
-      res += String.fromCharCode(digit)
-    }
-    return res
-  }
-
-  static bin2String (array) {
-    var result = ''
-    for (var i = 0; i < array.length; i++) {
-      result += String.fromCharCode(parseInt(array[i], 2))
-    }
-    return result
-  }
-
-  static dec2Bin (d) {
-    var b = ''
-
-    for (var i = 0; i < 8; i++) {
-      b = (d % 2) + b
-      d = Math.floor(d / 2)
-    }
-
-    return b
-  }
 }
