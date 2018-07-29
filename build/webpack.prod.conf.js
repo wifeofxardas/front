f6f06b309bce854ed6c640132cd24f1987d35306
wifeofxardas <thexardass@gmail.com>
2018-07-29 12:18:58 +0300

    fix public path

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -24,7 +24,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
     path: config.build.assetsRoot,
-    publicPath: 'front/',
+    publicPath: '/front/',
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
